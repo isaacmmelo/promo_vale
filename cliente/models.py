39403478472b7c0d3dd3517519d69ea85acbeb7a
isaacmmelo <isaac.melo@proton.me>
2023-10-04 18:47:50 -0300

    alterações tiago

--- cliente/models.py
+++ cliente/models.py
@@ -1,10 +1,40 @@
 from django.db import models
 
-# Create your models here.
+class Telefone(models.Model):
+    numero = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.numero
+
+class WhatsApp(models.Model):
+    telefone = models.CharField(max_length=15)
+    
+    def __str__(self):
+        return self.telefone
 class Cliente(models.Model):
+    email = models.EmailField(default='example@example.com')
     nome = models.CharField(max_length=150)
-    telefone = models.CharField(max_length=15, blank=True, null=True)
-    whatsapp = models.BooleanField(default=False)
-    sexo = models.CharField(max_length=10, choices=[('masculino', 'Masculino'), ('feminino', 'Feminino')], blank=True, null=True)
+    telefone = models.ForeignKey(
+        Telefone,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        verbose_name='Telefone'
+    )
+    whatsapp = models.ForeignKey(
+        WhatsApp,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        verbose_name='WhatsApp'
+    )
+    sexo = models.CharField(
+        max_length=10,
+        choices=[('masculino', 'Masculino'), ('feminino', 'Feminino')],
+        blank=True,
+        null=True
+    )
     senha_adicional = models.CharField(max_length=128, blank=True, null=True)
 
+    def __str__(self):
+        return self.nome
--- cliente/views.py
+++ cliente/views.py
@@ -1,64 +1,76 @@
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
-from .models import Cliente
+from django.contrib.auth import logout
+from .models import Cliente, Telefone
+
 
 def cadastro(request):
     return render(request, 'cliente/cadastro.html')
+
 
 def cadastrar(request):
     if request.method == 'POST':
         # Captura dos dados do formulário
         email = request.POST["email"]
         senha = request.POST["senha"]
-        telefone = request.POST["telefone"]
-        whatsapp = request.POST.get("whatsapp", False)  # Pode ser "Sim" ou não definido
-        sexo = request.POST.get("genero", "")  # Pode ser "Masculino" ou "Feminino"
+        numero_telefone = request.POST["numero_telefone"]
+        # Pode ser "Sim" ou não definido
+        whatsapp = request.POST.get("whatsapp", False)
+        # Pode ser "Masculino" ou "Feminino"
+        sexo = request.POST.get("genero", "")
 
         # Verifica se o usuário já existe
         if Cliente.objects.filter(email=email).exists():
             msg = "Erro: Este email já está cadastrado."
             return render(request, 'cliente/cadastro.html', {'msg': msg})
-        
-        cliente = Cliente
-        cliente.email = email
-        cliente.senha_adicional = senha
-        cliente.telefone = telefone
-        cliente.whatsapp = whatsapp
-        cliente.sexo = sexo
+
+        telefone_obj, created = Telefone.objects.get_or_create(numero=numero_telefone)
+        cliente = Cliente(email=email, senha_adicional=senha,
+                          telefone=telefone_obj, whatsapp=whatsapp, sexo=sexo)
 
         try:
-            resultado = cliente.save()
+            cliente.save()
         except:
             msg = "Erro no cadastro, tente novamente"
-            return render(request, 'cliente/cadastro.html', {'msg': msg}) 
+            return render(request, 'cliente/cadastro.html', {'msg': msg})  
+             
         else:
             msg = "Cadastro realizado com sucesso! Faça o login"
             return render(request, 'cliente/login.html', {'msg': msg}) 
+
 
 def fazer_login(request):
     if request.method == 'POST':
         # Processar os dados do formulário de login aqui
         email = request.POST['email']
         senha = request.POST['senha']
-               
+
         if Cliente.objects.filter(email=email).exists():
-            cliente = Cliente.get(email=email)
-            if (senha == cliente.senha):
-                #logar
+            cliente = Cliente.objects.get(email=email)
+            if senha == cliente.senha_adicional:
+                # Logar com sucesso
+                # Você pode adicionar lógica de autenticação aqui
                 print('logado')
-                #redirecion para a página inicial
+                # Redirecionar para a página inicial ou outra página de destino
+                return HttpResponseRedirect(reverse('pagina_inicial'))
             else:
-                #retornar senha incorreta
+                # Retornar senha incorreta
                 msg = "Senha incorreta"
                 return render(request, 'cliente/login.html', {'msg': msg})
         else:
-            #retornar usuario não cadastrado
-            msg = "Usuario não encontrado"
-            return render(request, 'cliente/login.html', {'msg': msg}) 
+            # Retornar usuário não cadastrado
+            msg = "Usuário não encontrado"
+            return render(request, 'cliente/login.html', {'msg': msg})
+    else:
+        # Se não for uma solicitação POST, renderizar a página de login
+        return render(request, 'cliente/login.html')
+
 
 def perfil(request):
     # A função de perfil requer que o usuário esteja autenticado
     return render(request, 'cliente/perfil.html')
+
 
 def fazer_logout(request):
     logout(request)
